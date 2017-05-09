424bf80122bb8d5a3d705efd0687655f7b0c1d8c
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2017-05-09 19:34:04 -0300

    git init

---
+++ cmake-build-debug/CMakeFiles/TPCuatrimestral.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TPCuatrimestral.dir/main.c.obj"
+  "TPCuatrimestral.pdb"
+  "TPCuatrimestral.exe"
+  "TPCuatrimestral.exe.manifest"
+  "libTPCuatrimestral.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TPCuatrimestral.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
