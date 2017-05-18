235014f506e1fa8c4d5a8bb30c463ae99ac05699
Nacho <ignacio.delavega@ing.asutral.edu.ar>
2017-05-18 14:16:13 -0300

    Added new functions to Table

--- Exercise2/CameraAndAccesoryTable.h
+++ Exercise2/CameraAndAccesoryTable.h
@@ -20,6 +20,9 @@ CameraAndAccesoryTable* newCameraAndAccesoryTable();
 void freeCameraAndAccesoryTable(CameraAndAccesoryTable* cameraAndAccesoryTable);
 void addCameraToTable(CameraAndAccesoryTable* cameraAndAccesoryTable,Camera* camera);
 void addAccesoryToTable(CameraAndAccesoryTable* cameraAndAccesoryTable,Accesory* accesory);
-
+int isCamera(CameraAndAccesoryTable* cameraAndAccesoryTable,int code);
+int isAccesory(CameraAndAccesoryTable* cameraAndAccesoryTable,int code);
+Camera* getCamera(CameraAndAccesoryTable* cameraAndAccesoryTable, int code);
+Accesory* getAccesory(CameraAndAccesoryTable* cameraAndAccesoryTable,int code);
 
 #endif //TPCUATRIMESTRAL_CAMERAANDACCESORYTABLE_H
