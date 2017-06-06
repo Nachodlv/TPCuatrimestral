f3880c5601fe2da97aac66341e3f5c099c90ebaf
Nacho <ignacio.delavega@ing.asutral.edu.ar>
2017-06-06 09:59:19 -0300

    Finish

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
-#include "Exercise1/ExecuteExercise1.h"
-#include "Exercise2/ExecuteExercise2.h"
-#include "Exercise3/ExecuteExercise3.h"
-#include "Exercise4/ExecuteExercise4.h"
-#include "Exercise5/ExecuteExercise5.h"
+#include <stdio.h>
+#include "src/Exercise1/ExecuteExercise1.h"
+#include "src/Exercise2/ExecuteExercise2.h"
+#include "src/Exercise3/ExecuteExercise3.h"
+#include "src/Exercise4/ExecuteExercise4.h"
+#include "src/Exercise5/ExecuteExercise5.h"
 
 int main(){
     //executeExercise1();
     //executeExercise2();
     //executeExercise3();
     //executeExercise4();
-    executeExercise5();
+    //executeExercise5();
 }
